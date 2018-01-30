f9444cacb5e2de1b950b6952a63113a5c87e8bed
PaulODonovan <blueprintdesignmail@yahoo.com>
2018-01-30 14:45:15 +0100

    adds test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,9 +3,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+	text: 'First test todo'
+},{
+	text: 'Second test todo'
+}];
+
 // out tests below assume the database to be empty. So before each test, we need to empty it
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());//expression syntax
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());//expression syntax
 });
 
 describe('POST /todos', () => {
@@ -26,7 +34,7 @@ describe('POST /todos', () => {
 				}
 
 				// verify that it's in the database
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					// make some assertions
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
@@ -47,10 +55,22 @@ describe('POST /todos', () => {
 					return done(err)
 				}
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2);
 					done();
 				}).catch((e) => done(e));
 			})
 	});
 
+});
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
+	});
 });
