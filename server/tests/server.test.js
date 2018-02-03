89832183575d5a6b2dd037a8a75111d41f825054
PaulODonovan <blueprintdesignmail@yahoo.com>
2018-02-04 00:42:55 +0100

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -224,7 +224,7 @@ describe('POST /users', () => {
 					expect(user).toBeTruthy();
 					expect(user.password).not.toBe(password);
 					done();
-				});				
+				}).catch((e)=>done(e));				
 			});
 	});
 
@@ -246,5 +246,66 @@ describe('POST /users', () => {
 			.send({email, password})
 			.expect(400)
 			.end(done);
+	});
+});
+
+describe('POST /users/login', () => {
+	it('should login user and return auth token', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password : users[1].password
+			})
+			.expect(200)
+			.expect((res) => {
+				// (use bracket notation because of the hyphen)
+				expect(res.headers['x-auth']).toBeTruthy();
+			})
+			.end((err, res) => {
+				if(err){
+					return done(err);
+				}
+				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens[0])
+
+					// .toInclude
+
+					// .toMatchObject({
+					// 	access : 'auth',
+					// 	token: res.headers['x-auth']
+					// });
+
+					.toEqual(
+			            expect.objectContaining({
+				            access: 'auth',
+				            token: res.headers['x-auth']
+			          })
+			        );
+
+					done();
+				}).catch((e)=>done(e));
+			})
+	});
+	it('should reject invalid login', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password : 'badpw' // ***********INVALID PW GOES HERE**********
+			})
+			.expect(400)
+			.expect((res) => {
+				expect(res.headers['x-auth']).not.toBeTruthy();
+			})
+			.end((err, res) => {
+				if(err){
+					return done(err);
+				}
+				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens.length === 0);
+					done();
+				}).catch((e)=>done(e));
+			})
 	});
 });
