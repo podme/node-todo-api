84eb0088216c2ffe184e2952b44b87dbcded3f29
PaulODonovan <blueprintdesignmail@yahoo.com>
2018-02-04 17:19:06 +0100

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,9 @@
 var env = process.env.NODE_ENV || 'development';
-// console.log('env: ', JSON.stringify(env));
-if(env==='development'){
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 
-}else if(env==='test'){
-	console.log("env==='test'");
-	process.env.PORT = 3000;
-	// use a different database for testing so tests don't wipe database
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if(env==='development' || env==='test'){
+	var config = require('./config.json');
+	var envConfig = config[env];
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key]
+	});
 }
-// console.log('process.env.PORT: ', process.env.PORT);
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,7 +12,7 @@ const users = [{
 	password: 'poopypoops',
 	tokens: [{
 		access: 'auth',
-		token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123--*secret').toString()
+		token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
 	}]
 },{
 	_id: userTwoId,
@@ -20,7 +20,7 @@ const users = [{
 	password: 'nug-nug',
 	tokens: [{
 		access: 'auth',
-		token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123--*secret').toString()
+		token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
 	}]
 }];
 
